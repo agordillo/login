6fa9fd68ae9b30e983ca41afffcc361bee250a2d
Clara Salgado <clara7227@adaptador-1.dit.upm.es>
2025-11-03 13:55:55 +0100

    added style

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,5 +1,19 @@
 import "./../assets/scss/MainScreen.scss";
+// import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 
 export default function MainScreen({ config, solved }) {
-  return <div className="frame">login</div>;
+  return <div className="frame">
+    <div className="containerLogin">
+<div className="imgAvatar"></div>
+<h2>Name of user</h2>
+<input class="input" name="text" placeholder="Search..." type="search"></input>
+ <p> I forgot my PIN</p>
+ </div> 
+ 
+ <div className="footer">
+  {/* <PowerSettingsNewIcon/> */}
+  <i>accesibilidad</i>
+  <i>login</i>
+ </div>
+ </div>;
 }
